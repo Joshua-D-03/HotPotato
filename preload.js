2ebb0b18204e11a875c484ec32b27e8ac35cb5fb
Joshua-D-03 <jdanclair2003@gmail.com>
2026-03-13 11:15:38 -0400

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,6 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('electronAPI', {
+contextBridge.exposeInMainWorld('api', {
     sendCompress: (data) => ipcRenderer.send('compress-game', data),
     onCompressResult: (callback) => ipcRenderer.on('compression-result', (event, ...args) => callback(...args)),
     selectFolder: () => ipcRenderer.invoke('dialog:openFolder')
