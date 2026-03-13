027a1d09cf34a20a1e7e822965737c72de5f0a70
Joshua-D-03 <jdanclair2003@gmail.com>
2026-03-13 11:08:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-
-function createWindow(remoteMain.enable(win.webContents);) {
-  const win = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  });
-
-  win.loadFile('index.html');
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'), // Point to your bridge
+            contextIsolation: true, // Keep this true for security
+            nodeIntegration: false
+        }
+    });
+    win.loadFile('index.html');
 }
-
-app.whenReady().then(createWindow);
