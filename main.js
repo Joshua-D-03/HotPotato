3561b4ac11c627eee64ff473a4435310a689d50a
Joshua-D-03 <jdanclair2003@gmail.com>
2026-03-13 11:41:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,14 +5,16 @@ function createWindow() {
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'), // Point to your bridge
             contextIsolation: true, // Keep this true for security
-            nodeIntegration: false
-        }
+            nodeIntegration: false      
+}
     });
     win.loadFile('index.html');
-}
 const { ipcMain, dialog } = require('electron');
 
 ipcMain.handle('dialog:openFolder', async () => {
     const result = await dialog.showOpenDialog({ properties: ['openDirectory'] });
     return result.filePaths[0];
 });
+
+app.whenReady().then(createWindow);
+}
